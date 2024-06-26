3be6a1acfb774345fe9c5e59abf22f8c06a179d7
goldenjeffempire <jeffemuodafe124@gmail.com>
2024-06-26 15:02:53 +0100

    Block scoped js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function
+taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    let task = true; // eslint-disable-line no-unused-vars
+    let task2 = false; // eslint-disable-line no-unused-vars
+  }
+
+  return [task, task2];
+}
